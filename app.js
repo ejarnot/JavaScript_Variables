d4e0a5c184cba2e62e2530115cd40bd7330e1b8d
Mia Jarnot <ejarnot42@outlook.com>
2022-03-22 12:22:25 -0700

    Told you about myself using JS!

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Mia";
+let lastName = "Jarnot";
+var age = 30;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
 // Exercise 3
+let city = "Portland, OR";
+let favoritePasttime = "Ultimate Frisbee";
+let myStory = `Hello! My name is ${fullName}! I live in ${city} and I love to play ${favoritePasttime}.`;
+console.log(myStory)
